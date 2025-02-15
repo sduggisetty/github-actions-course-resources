93fa86848e8a0e0455d931ae24227a932e346a5f
sduggisetty <subbaraoemail@gmail.com>
2025-02-14 22:27:10 -0500

    Update MainContent.test.jsx

--- Code/02 Basics/02 Starting Project/src/components/MainContent.test.jsx
+++ Code/02 Basics/02 Starting Project/src/components/MainContent.test.jsx
@@ -17,5 +17,5 @@ describe('MainContent', () => {
     const button = screen.getByRole('button');
     await userEvent.click(button);
     expect(screen.getByTestId('help-area')).toBeInTheDocument();
-  });
+  })
 });
